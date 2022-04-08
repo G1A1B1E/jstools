1863766ca8fce77aebeece06ce374d9d06e1c40c
Gabe Macri <gbrlmacri@gmail.com>
2022-04-08 08:59:06 -0700

    calc

--- calc.js
+++ calc.js
@@ -1,7 +1,26 @@
-function calculate_this() {
-    var input = document.getElementById('numbers').value;
-    var results = eval(input);
-    document.getElementById('results').innerHTML = results;
-  }
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map( button => {
+  button.addEventListener('click', (e) => {
+      switch(e.target.innerText){
+          case 'C':
+              display.innerText = '';
+              break;
+          case '=':
+              try{
+                  display.innerText = eval(display.innerText);
+              } catch {
+                  display.innerText = "Error"
+              }
+              break;
+          case '←':
+              if (display.innerText){
+                 display.innerText = display.innerText.slice(0, -1);
+              }
+              break;
+          default:
+              display.innerText += e.target.innerText;
+      }
+  });
+});
 
-  
